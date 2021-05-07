cea8f323f6be8d2de117ff64cf8ecb3c8e0f49c5
William Andres Delgado Murcia <wiilliiam.delgado@gmail.com>
2021-05-06 19:18:18 -0500

    update

--- apiintermedio.api/Controllers/GenerarDocsController.cs
+++ apiintermedio.api/Controllers/GenerarDocsController.cs
@@ -24,11 +24,12 @@ namespace apiintermedio.api.Controllers
                 var tokenNoBearer = tokenReturn.Split(' ');
                 var urlApi = constants.OLIMPIA_URL;
                 var path = urlApi + constants.OLIMPIA_PATH_DOCUMENTOS;
+
                 /*var json2 = "{\"procesoClienteId\":\"william123\",\"numeroAprobacion\":\"1\",\"tipoEndoso\":2,\"requiereATDP\":false,\"identificacionComercio\":9008397029,\"plantillas\":[{\"plantillaCodigo\":\"2FE21B47-2D3C-43CD-B483-A9FED82B46AE\",\"tipoDocumento\":3,\"camposPlantilla\":[{\"campoNombre\":\"NomGarante\",\"campoValor\":\"Juan\"},{\"campoNombre\":\"NumDocGarante\",\"campoValor\":\"101010101\"}]}],\"firmantes\":[{\"correoElectronico\":\"wiilliiam.delgado@gmail.com\",\"celular\":33232915235,\"nombre\":\"william\",\"apellido\":\"delgado\",\"identificacion\":\"1014294090\",\"tipoIdentificacion\":1,\"campoFirma\":\"FirmDeudor1\",\"tipoInvolucrado\":1},{\"correoElectronico\":\"sergio@gmail.com\",\"celular\":3002121509,\"nombre\":\"sergio\",\"apellido\":\"lopez\",\"identificacion\":\"101429409\",\"tipoIdentificacion\":1,\"campoFirma\":\"FirmDeudor2\",\"tipoInvolucrado\":2}]}";
                 var dato2 = api.GetApiHttpPost(path, json2, tokenNoBearer[1]);
                 string responseOlimpia2 = dato2;*/ 
 
-                return Ok(tokenNoBearer[1]);
+                return Ok("Los documentos se enviaron de manera exitosa");
             }
             catch (Exception ex)
             {
--- apiintermedio.api/Controllers/LoginController.cs
+++ apiintermedio.api/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
 ﻿using apiintermedio.api.Services;
+using apiintermedio.api.Constants;
 using apiintermedio.core;
 using Newtonsoft.Json;
 using System;
@@ -16,22 +17,15 @@ namespace apiintermedio.api.Controllers
         {
             try
             {
+                constantsOlimpia constants = new constantsOlimpia();
                 CoreLogin coreLogin = new CoreLogin();
                 ConsumoApi api = new ConsumoApi();
                 var datos = coreLogin.traerInfoLogin();
-                var urlApi = "https://reconoserpruebas.olimpiait.com:6407";
-                var path = urlApi + "/Login";
+                var urlApi = constants.OLIMPIA_URL;
+                var path = urlApi + constants.OLIMPIA_PATH_LOGIN;
                 var json = JsonConvert.SerializeObject(datos);
                 var dato = api.ConsumePostNoToken(path, json);
-                //var dato2 = api.GetApiHttpPost(path, json);
-                //var infoData = JsonConvert.DeserializeObject<string>(dato);
                 string responseOlimpia = dato;
-
-                var path2 = urlApi + "/Documento/AprobacionMultiplesDocumentos";
-                var json2 = "{\"procesoClienteId\":\"william123\",\"numeroAprobacion\":\"1\",\"tipoEndoso\":2,\"requiereATDP\":false,\"identificacionComercio\":9008397029,\"plantillas\":[{\"plantillaCodigo\":\"2FE21B47-2D3C-43CD-B483-A9FED82B46AE\",\"tipoDocumento\":3,\"camposPlantilla\":[{\"campoNombre\":\"NomGarante\",\"campoValor\":\"Juan\"},{\"campoNombre\":\"NumDocGarante\",\"campoValor\":\"101010101\"}]}],\"firmantes\":[{\"correoElectronico\":\"wiilliiam.delgado@gmail.com\",\"celular\":33232915235,\"nombre\":\"william\",\"apellido\":\"delgado\",\"identificacion\":\"1014294090\",\"tipoIdentificacion\":1,\"campoFirma\":\"FirmDeudor1\",\"tipoInvolucrado\":1},{\"correoElectronico\":\"sergio@gmail.com\",\"celular\":3002121509,\"nombre\":\"sergio\",\"apellido\":\"lopez\",\"identificacion\":\"101429409\",\"tipoIdentificacion\":1,\"campoFirma\":\"FirmDeudor2\",\"tipoInvolucrado\":2}]}";
-                var dato2 = api.GetApiHttpPost(path2, json2, "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjQ2NzdmNzRmLWI3ZDgtNGEyMC04ZDgyLWU2NTZmNjU5YmRmYyIsIm5iZiI6MTYxOTYyOTczOCwiZXhwIjoxNjE5NzE2MTM4LCJpYXQiOjE2MTk2Mjk3Mzh9.PdSKyJYJVjk5CTwR4Bi6kOV44DzbSidJE6dtEb6N7Eo");
-                string responseOlimpia2 = dato2;
-                var infoData = JsonConvert.DeserializeObject<string>(responseOlimpia2);
                 return Ok(responseOlimpia);
             }
             catch (Exception ex)
